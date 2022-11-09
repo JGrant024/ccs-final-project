c0b69e677fc4e31d753d8d43e8b3bf9f2e765732
JGrant024 <jonathan.grant23@icloud.com>
2022-11-09 10:54:45 -0500

    updated UserProfiles and updated Map to display

---
+++ frontend/static/src/pages/Map.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { GoogleMap } from "@react-google-maps/api";
+
+function Map(props) {
+  const [mapContainer, setMapContainer] = useState(null);
+
+  return (
+    <GoogleMap
+      mapContainerStyle={props.mapContainerStyle}
+      center={props.center}
+      zoom={props.zoom}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  );
+}
+
+export default Map;
--- frontend/static/src/pages/UserProfile.js
+++ frontend/static/src/pages/UserProfile.js
@@ -1,48 +1,25 @@
-import React, { useRef, useEffect } from "react";
-import { GoogleMap, useLoadScript, useGoogleMap } from "@react-google-maps/api";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React from "react";
+import { LoadScript } from "@react-google-maps/api";
+import Map from "./Map";
 
-const libraries = ["places"];
+const mapContainerStyle = {
+  width: "400px",
+  height: "400px",
+};
 
-function Map() {
-  // Loads the map using API KEY
-  const latLng = new window.google.maps.LatLng(34.8526, 82.394);
-
-  var mapOptions = {
-    center: latLng,
-    zoom: 15,
-    mapTypeId: window.google.maps.MapTypeId.ROADMAP,
-  };
-
-  var infoWindow = new window.google.maps.InfoWindow();
-
-  // var map = new window.google.maps.Map(
-  //   document.getElementById("map"),
-  //   mapOptions
-  // );
-
-  console.log(document.getElementById("map"));
-
-  console.log(mapOptions);
-  // const { isLoaded } = useLoadScript({
-  //   googleMapsApiKey: "AIzaSyABLxEcVSAK2EWpfE3VSI5zS_3MwFSB_X4",
-  //   libraries,
-  // });
-
-  // if (!isLoaded) return <div>Loading...</div>;
-
-  return <div id="map">hi</div>;
-}
+const center = {
+  lat: 34.8526,
+  lng: -82.394,
+};
 
 function UserProfile() {
-  console.log(document.getElementById("map"));
   return (
-    <Wrapper apiKey="AIzaSyABLxEcVSAK2EWpfE3VSI5zS_3MwFSB_X4">
-      <div>
-        <Map />
-      </div>
-    </Wrapper>
+    <LoadScript googleMapsApiKey="AIzaSyABLxEcVSAK2EWpfE3VSI5zS_3MwFSB_X4">
+      <Map mapContainerStyle={mapContainerStyle} center={center} zoom={10} />
+    </LoadScript>
   );
 }
+
+// export default React.memo(MyComponent)
 
 export default UserProfile;

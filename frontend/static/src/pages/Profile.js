ff38cad80d26b9c83966da9e4ad743ea11d4513c
JGrant024 <jonathan.grant23@icloud.com>
2022-11-19 15:53:19 -0500

    removed extra buttons from testting

--- frontend/static/src/pages/Profile.js
+++ frontend/static/src/pages/Profile.js
@@ -74,12 +74,7 @@ const Profile = (props) => {
         {<section />}
         
       <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
+   
       <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
         <button onClick={() => console.log("testing")} className="btn">
           Follow
